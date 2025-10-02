e04f8bf90466c009627cda2b51ef51b03af00e19
Oludayo Fayemi <fayemi.oludayo@gmail.com>
2025-10-02 21:12:01 +0200

    new component: header

---
+++ app/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="px-5 fixed">
+        <div class=" header-initials flex flex-row justify-between">
+            <NuxtLink to="/"><p>Oludayo Fayemi</p></NuxtLink>
+        </div>
+    </header>
+  </template>
+  
