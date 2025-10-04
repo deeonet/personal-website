d3edee14ac864bf31d9ecae5a998ed9ab8e9d703
Oludayo Fayemi <fayemi.oludayo@gmail.com>
2025-10-04 10:35:03 +0200

    animation for footer content

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="w-full bottom-0 h-18 pt-10">
         <div class="footer container p-4 flex flex-col justify-center items-center pt-10">
-            <div class="flex flex-col items-center justify-center">
+            <div class="flex flex-col items-center justify-center animate-pulse">
                 <p class="md:text-xl text-xs sm:text-sm">Let's connect - We can do without forms :)</p>
                 <a class="header-initials text-md" href="mailto:hello@oludayofayemi.com">Email me</a>
             </div>
