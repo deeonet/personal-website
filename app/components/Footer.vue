12662fe56a43098d9dd98872034c820709111b82
Oludayo Fayemi <fayemi.oludayo@gmail.com>
2025-10-04 08:57:34 +0200

    new footer layout design

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
-    <footer class="container footer">
-      <p>Let's connect - We can do without forms :)</p>
-      <div >
-        <a class="btn" href="mailto:hello@oludayofayemi.com">Email me</a>
-      </div>
-      <div style="margin-top:18px; font-size:13px; color:var(--muted);">© 2025 | Oludayo Fayemi | hello@oludayofayemi.com</div>
-    </footer>
+    <footer class="w-full bottom-0 h-18 pt-10">
+        <div class="footer container p-4 flex flex-col justify-center items-center pt-10">
+            <div class="flex flex-col items-center justify-center">
+                <p class="md:text-xl text-xs sm:text-sm">Let's connect - We can do without forms :)</p>
+                <a class="header-initials text-md" href="mailto:hello@oludayofayemi.com">Email me</a>
+            </div>
+            <div class="flex flex-row justify-between pt-10 md:pt-20 md:text-xl text-xs sm:text-sm">© 2025 | Oludayo Fayemi | hello@oludayofayemi.com</div>
+        </div>
+        </footer>
   </template>
   
